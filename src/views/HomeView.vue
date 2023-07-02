a2679ec5fba3c2729460790abc40c5fbdbec84c3
馬國晉 <chan2132te@gmail.com>
2023-07-02 17:22:44 +0800

    feat: 找朋友 登入

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,54 @@
 <template>
   <div class="flex">
     <div id="Header" class="fixed w-[420px] z-10">
-      <div class="bg-[#F0F0F0] w-full flex justify-between items-center px-3 py-2">
-        <img class="rounded-full ml-1 w-10 h-10" :src="userStore.picture || ''" alt="" />
-        <div class="flex item-center justify-center">
+      <div
+        class="bg-[#F0F0F0] w-full flex justify-between items-center px-3 py-2"
+      >
+        <img
+          class="rounded-full ml-1 w-10"
+          :src="userStore.picture || ''"
+          alt=""
+        />
+        <div class="flex items-center justify-center">
           <AccountGroupIcon fillColor="#515151" class="mr-6" />
-          <DotsVerticalIcon @click="logout" fillColor="#515151" class="cursor-pointer" />
+          <DotsVerticalIcon
+            @click="logout"
+            fillColor="#515151"
+            class="cursor-pointer"
+          />
         </div>
       </div>
+
       <div id="Search" class="bg-white w-full px-2 border-b shadow-sm">
-        <div class="px-1 m-2 flex items-center justify-center bg-[#F0F0F0] rounded-md">
-          <MagnifyIcon fillColor="#515151" class="ml-2" :size="18" />
-          <input type="text"
-            class="bg-[#F0F0F0] ml-5 appearance-none w-full py-1.5 px-1.5 text-gray-700 leading-tught focus:outline-none focus:shadow-outline placeholder:text-sm placeholder:text-gray-500"
-            autocomplete="off" placeholder="開始聊天" />
+        <div
+          class="px-1 m-2 bg-[#F0F0F0] flex items-center justify-center rounded-md"
+        >
+          <MagnifyIcon fillColor="#515151" :size="18" class="ml-2" />
+          <input
+            @click="showFindFriends = !showFindFriends"
+            class="ml-5 apperance-none w-full bg-[#F0F0F0] py-1.5 px-2.5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder:text-sm placeholder:text-gray-500"
+            autocomplete="off"
+            type="text"
+            placeholder="Start a new chat"
+          />
         </div>
       </div>
     </div>
+
     <div v-if="showFindFriends">
-      <ChatsView class="mt-[100px]" />
-    </div>
-    <div v-else>
       <FindFriendsView class="pt-28" />
     </div>
-    <div v-if="open">
+    <div v-else>
+      <ChatsView class="mt-[100px]" />
+    </div>
+
+    <div v-if="userDataForChat.length">
       <MessageView />
     </div>
     <div v-else>
-      <div class="ml-[420px] fixed w-[calc(100vw-420px)] h-[100vh] bg-gray-100 text-center">
+      <div
+        class="ml-[420px] fixed w-[calc(100vw-420px)] h-[100vh] bg-gray-100 text-center"
+      >
         <div class="grid h-screen place-items-center">
           <div>
             <div class="w-full flex items-center justify-center">
@@ -42,7 +63,7 @@
               </div>
               <div>
                 Use WhatsApp on up to 4 linked devices and 1 phone at the same
-                time
+                time.
               </div>
             </div>
           </div>
@@ -53,34 +74,35 @@
 </template>
 
 <script setup>
-import {onMounted,ref} from 'vue'
-import { useUserStore } from '../store/user-store';
-import {useRouter} from 'vue-router';
-const userStore = useUserStore()
-const router = useRouter();
 import ChatsView from "./ChatsView.vue";
 import FindFriendsView from "./FindFriendsView.vue";
 import MessageView from "./MessageView.vue";
 import AccountGroupIcon from "vue-material-design-icons/AccountGroup.vue";
 import DotsVerticalIcon from "vue-material-design-icons/DotsVertical.vue";
 import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import { onMounted } from "vue";
+import { useUserStore } from "../store/user-store";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
 
+const userStore = useUserStore();
+const router = useRouter();
+const { showFindFriends, userDataForChat } = storeToRefs(userStore);
 
-let open = ref(true);
-let showFindFriends = ref(false);
-const logout = ()=>{
-  let res = confirm('你確定要離開嗎?')
-  if (res) userStore.logout(); router.push('/login')
-}
+const logout = () => {
+  let res = confirm("你確定要離開嗎?");
+  if (res) userStore.logout();
+  router.push("/login");
+};
 
-
-onMounted(() => {
+onMounted(async () => {
   try {
-      userStore.getAllUsers()
+    userStore.getAllUsers();
+    await userStore.getAllChatsByUser();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}),
+});
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="w-full">
-    <div class="bg-teal-600 z-[-1] w-full h-[225px] fixed top-0"></div>
-    <div class="bg-[#191919] z-[-1] w-full h-[calc(100vh-225px)] fixed bottom-0"></div>
-    <div class="max-w-xl mx-auto">
-      <div class="mt-10 flex items-center w-full">
-        <img src="whatsapp-logo.png" width="40" alt="" />
-        <div class="font-semibold text-gray-100 ml-6">WHATSAPP WEB</div>
+      <div class="bg-teal-600 z-[-1] w-full h-[225px] fixed top-0"></div>
+      <div class="bg-[#191919] z-[-1] w-full h-[calc(100vh-225px)] fixed bottom-0"></div>
+
+      <div class="max-w-xl mx-auto">
+          <div class="mt-10 flex items-center w-full">
+              <img width="40" src="whatsapp-logo.png" alt="">
+              <div class="font-semibold text-gray-100 ml-6">WHATSAPP WEB</div>
+          </div>
+
+          <div class="bg-white z-10 p-24 m-6 mt-10">
+              <div class="text-center text-4xl text-gray-700 font-light pb-10">
+                  WhatsApp Clone
+              </div>
+              <div class="w-full flex justify-center bg-[#fff] p-3 rounded-md">
+                  <GoogleLogin :callback="callback"/>
+              </div>
+          </div>
       </div>
-      <div class="bg-white z-10 p-24 m-6 mt-10">
-        <div class="text-center text-4xl text-gray-700 font-light pb-10">
-          WhatsApp Clone
-        </div>
-        <div class="w-full flex justify-center bg-[#191919] p-3 rounded-md">
-          <GoogleLogin :callback="callback" />
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
-import { useUserStore } from '../store/user-store';
-import {useRouter} from 'vue-router'
-const router = useRouter();
+import { useUserStore } from '../store/user-store'
+import { useRouter } from 'vue-router'
 const userStore = useUserStore()
+const router = useRouter()
 
-const callback = async (response) =>{
-  console.log(response);
+const callback = async (response) => {
   await userStore.getUserDetailsFromGoogle(response)
-  setTimeout(()=>{
-      router.push('/');
-  },200)
-} 
+  setTimeout(() => { router.push('/') }, 200)
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
