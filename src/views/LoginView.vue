9eb826bd895f9cc56bf65cae1618ecce9063ca66
馬國晉 <chan2132te@gmail.com>
2024-07-25 18:36:06 +0800

    更新loginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full">
+  <div class="w-full h-full overflow-hidden">
     <div class="bg-teal-600 z-[-1] w-full h-[225px] fixed top-0"></div>
     <div
       class="bg-[#191919] z-[-1] w-full h-[calc(100vh-225px)] fixed bottom-0"
